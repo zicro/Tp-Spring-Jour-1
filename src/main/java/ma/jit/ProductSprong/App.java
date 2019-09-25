9f01c181fd727faa667afab0b8e4c2f2b432d765
HABACHI <rabiie.habachi@gmail.com>
2019-09-25 23:36:20 +0100

    first commit

---
+++ src/main/java/ma/jit/ProductSprong/App.java
@@ -0,0 +1,13 @@
+package ma.jit.ProductSprong;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
